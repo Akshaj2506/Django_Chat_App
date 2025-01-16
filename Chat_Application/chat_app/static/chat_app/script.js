bf681e7871b13fd860b3ba51041e235342c36d3b
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2025-01-16 13:09:13 +0530

    auth forms redesigned

--- Chat_Application/chat_app/static/chat_app/script.js
+++ Chat_Application/chat_app/static/chat_app/script.js
@@ -4,3 +4,27 @@ document.getElementById("toggle-sidebar").addEventListener("click", () => {
    document.getElementById("right-area").classList.toggle("collapsed")
    document.getElementById("users-menu").classList.toggle("vanish")
 })
+
+function adjustPageSize() {
+   const screenWidth = window.innerWidth;
+   const body = document.body;
+
+   body.style.transform = "";
+   body.style.transformOrigin = "top left";
+
+   if (screenWidth > 992 && screenWidth <= 1600) {
+      body.style.transform = "scale(0.9)";
+   } else if (screenWidth >= 768 && screenWidth <= 991) {
+      body.style.transform = "scale(0.85)";
+   } else if (screenWidth >= 700 && screenWidth <= 767) {
+      body.style.transform = "scale(0.8)";
+   } else if (screenWidth >= 600 && screenWidth < 700) {
+      body.style.transform = "scale(0.75)";
+   } else if (screenWidth <= 600) {
+      body.style.transform = "scale(0.5)";
+   }
+}
+
+window.addEventListener("resize", adjustPageSize);
+
+adjustPageSize();
