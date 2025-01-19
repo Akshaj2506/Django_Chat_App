5ee6786d253e83ca6be345f40491c5f464ac82ef
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2025-01-19 15:26:26 +0530

    turned off debug mode

--- Chat_Application/Chat_Application/settings.py
+++ Chat_Application/Chat_Application/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^yf_i6hm&(mpa(4nh!x4+tu6zj=1#hr*poz=ikz9$1kq@#(28'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django-chat-app-rzzq.onrender.com'
