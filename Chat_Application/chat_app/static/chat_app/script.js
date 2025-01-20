0746b7d43807e1fc0519f3e3ee0527dd235394be
Akshaj2506 <74644487+Akshaj2506@users.noreply.github.com>
2025-01-20 12:11:45 +0530

    made some css changes

--- Chat_Application/chat_app/static/chat_app/script.js
+++ Chat_Application/chat_app/static/chat_app/script.js
@@ -1,6 +1,6 @@
 document.getElementById("toggle-sidebar").addEventListener("click", () => {
    document.getElementById("user-menu-container").classList.toggle("collapsed")
-   document.getElementById("chat-area").classList.toggle("collapsed")
+   if (window.innerWidth >= 991) document.getElementById("chat-area").classList.toggle("collapsed")
    document.getElementById("right-area").classList.toggle("collapsed")
    document.getElementById("users-menu").classList.toggle("vanish")
 })
@@ -19,9 +19,11 @@ function adjustPageSize() {
    } else if (screenWidth >= 700 && screenWidth <= 767) {
       body.style.transform = "scale(0.8)";
    } else if (screenWidth >= 600 && screenWidth < 700) {
-      body.style.transform = "scale(0.75)";
+      body.style.transform = "scale(1)";
+      body.style.width = "75%";
    } else if (screenWidth <= 600) {
-      body.style.transform = "scale(0.5)";
+      body.style.transform = "scale(1)";
+      body.style.width = "50%";
    }
 }
 
